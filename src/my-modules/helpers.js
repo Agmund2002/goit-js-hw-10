0eb00cdaed5f8fc73f83efe7a6ead04ea27836a5
Agmund2002 <malysh.andrey2000@gmail.com>
2023-08-26 20:16:03 +0300

    stylized

--- src/my-modules/helpers.js
+++ src/my-modules/helpers.js
@@ -2,11 +2,11 @@ import { elements } from "./elements";
 
 function createMarkup(img, breed, description, temperament) {
     elements.box.innerHTML = `
-    <img src="${img}" alt="${breed}">
-    <div>
-        <h3>${breed}</h3>
-        <p>${description}</p>
-        <p><span>Temperament: </span>${temperament}</p>
+    <img class="cat-img" src="${img}" alt="${breed}">
+    <div class="cat-info-box">
+        <h3 class="cat-breed cat-info">${breed}</h3>
+        <p class="cat-info">${description}</p>
+        <p class="cat-info"><span class="bold">Temperament: </span>${temperament}</p>
     </div>`;
 }
 
